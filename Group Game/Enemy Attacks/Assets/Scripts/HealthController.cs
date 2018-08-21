d413a3a8a0ad3ffa20232483eac09fc1a4e5da09
Robert <robbie123.wow@gmail.com>
2018-08-21 19:50:22 +0200

    Redid the unity project added a default scene with some scripts
    Need to redo the UI

---
+++ Group Game/Enemy Attacks/Assets/Scripts/HealthController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour {
+
+	//This script updates the selected enemy's health to the UI system
+	
+	[Space]
+	[Header("UI elements")]
+	
+	public Text healthText;
+	public Slider healthBar;
+
+	public Player player;
+
+	private void FixedUpdate () {
+		if (player.targetedEnemy == null) 
+		{
+			
+			healthBar.gameObject.SetActive(false);
+
+		} else
+		{
+		
+			UpdateHealthUI();
+	
+		}
+	}
+
+	private void UpdateHealthUI()
+	{
+		healthBar.value = player.targetedEnemy.GetHealthPertcentage();
+		healthText.text = Mathf.Round(player.targetedEnemy.GetHealthPertcentage() * 100) + "%";
+	}
+}
