7f7791741178e9d2f7b245bb0a13e5f4ce0c0bf7
Robert <robbie123.wow@gmail.com>
2018-08-29 18:23:13 +0200

    Smoothed out some of the player movement and made the hiracy look better

--- Viking Game/Assets/Scripts/ThirdPersonCamera.cs
+++ Viking Game/Assets/Scripts/ThirdPersonCamera.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour {
 
-    float yaw;
-    float pitch;
-    public float mousesens = 10;
+    public bool lockCursor;
+    public float mouseSensitivity = 10;
     public Transform target;
     public float dstFromTarget = 2;
-    public Vector2 pitchminmax = new Vector2(-45, 85);
-    public float rotationSmoothtime = 0.12f;
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public float rotationSmoothTime = .12f;
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
-    
-	
-	void LateUpdate ()
-    {
-        yaw += Input.GetAxis("Mouse X") * mousesens;
-        pitch -= Input.GetAxis("Mouse Y") * mousesens;
-        pitch = Mathf.Clamp(pitch, pitchminmax.x, pitchminmax.y);
 
-        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(yaw, pitch), ref rotationSmoothVelocity, rotationSmoothtime);
+    float yaw;
+    float pitch;
+
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
         transform.eulerAngles = currentRotation;
 
         transform.position = target.position - transform.forward * dstFromTarget;
 
-	}
+    }
+
 }
